1e8f0818d24cc2ddde48ab2a956a7b829a200bb8
Lucas Correa <lucas.correa@etaure.com.br>
2021-09-29 09:42:28 -0300

    Entity Framework Core parte 1 - modulo 2 completo

--- Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
+++ Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -7,14 +8,10 @@ using System.Threading.Tasks;
 
 namespace Alura.Filmes.App.Negocio
 {
-    [Table("actor")]
     public class Ator
     {
-        [Column("actor_id")]
         public int Id { get; set; }
-        [Column("first_name")]
         public string PrimeiroNome { get; set; }
-        [Column("last_name")]
         public string UltimoNome { get; set; }
 
         public override string ToString()
