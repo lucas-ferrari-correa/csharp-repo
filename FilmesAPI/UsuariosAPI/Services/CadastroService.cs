136aa52ae2d04f9af0add380528745fecd7d7c11
Lucas Correa <lucas.correa@etaure.com.br>
2021-11-29 09:27:08 -0300

    errei

--- FilmesAPI/UsuariosAPI/Services/CadastroService.cs
+++ FilmesAPI/UsuariosAPI/Services/CadastroService.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using UsuariosAPI.Data.Dtos;
 using UsuariosAPI.Data.Request;
 using UsuariosAPI.Models;
@@ -15,11 +16,13 @@ namespace UsuariosAPI.Services
     {
         private IMapper _mapper;
         private UserManager<IdentityUser<int>> _userManager;
+        private EmailService _emailService;
 
-        public CadastroService(IMapper mapper, UserManager<IdentityUser<int>> userManager)
+        public CadastroService(IMapper mapper, UserManager<IdentityUser<int>> userManager, EmailService emailService)
         {
             _mapper = mapper;
             _userManager = userManager;
+            _emailService = emailService;
         }
 
         public Result CadastroUsuario(CreateUsuarioDto createUsuarioDto)
@@ -33,6 +36,14 @@ namespace UsuariosAPI.Services
             if (resultadoIdentity.Result.Succeeded)
             {
                 var code = _userManager.GenerateEmailConfirmationTokenAsync(usuarioIdentity).Result;
+                var encodedCode = HttpUtility.UrlEncode(code);
+
+                _emailService.EnviarEmail(
+                    new[] { usuarioIdentity.Email }, 
+                    "Link de Ativação",
+                    usuarioIdentity.Id,
+                    encodedCode
+                    );
                 return Result.Ok().WithSuccess(code);
             }
 
